a0e35d879baff0e530c1d1dba22fe8391ce4f511
0118Add <60157610+0118Add@users.noreply.github.com>
2025-10-02 23:38:08 +0800

    Update diy-master.sh

--- diy-master.sh
+++ diy-master.sh
@@ -18,12 +18,12 @@ fi
 
 color() {
     case $1 in
-        cr) echo -e "\e[1;31m$2\e[0m" ;;
-        cg) echo -e "\e[1;32m$2\e[0m" ;;
-        cy) echo -e "\e[1;33m$2\e[0m" ;;
-        cb) echo -e "\e[1;34m$2\e[0m" ;;
-        cp) echo -e "\e[1;35m$2\e[0m" ;;
-        cc) echo -e "\e[1;36m$2\e[0m" ;;
+        cr) echo -e "\e[1;31m$2\e[0m" ;;  # 红色
+        cg) echo -e "\e[1;32m$2\e[0m" ;;  # 绿色
+        cy) echo -e "\e[1;33m$2\e[0m" ;;  # 黄色
+        cb) echo -e "\e[1;34m$2\e[0m" ;;  # 蓝色
+        cp) echo -e "\e[1;35m$2\e[0m" ;;  # 紫色
+        cc) echo -e "\e[1;36m$2\e[0m" ;;  # 青色
     esac
 }
 
@@ -46,8 +46,6 @@ find_dir() {
 
 print_info() {
     printf "%s %-40s %s %s %s\n" $1 $2 $3 $4 $5
-    # read -r param1 param2 param3 param4 param5 <<< $1
-    # printf "%s %-40s %s %s %s\n" $param1 $param2 $param3 $param4 $param5
 }
 
 # 添加整个源仓库(git clone)
@@ -153,7 +151,7 @@ echo "REPO_URL=$REPO_URL" >>$GITHUB_ENV
 REPO_BRANCH="master"
 echo "REPO_BRANCH=$REPO_BRANCH" >>$GITHUB_ENV
 
-# 开始拉取编译源码
+# 拉取编译源码
 begin_time=$(date '+%H:%M:%S')
 [[ $REPO_BRANCH != "master" ]] && BRANCH="-b $REPO_BRANCH --single-branch"
 cd /workdir
@@ -163,7 +161,7 @@ ln -sf /workdir/openwrt $GITHUB_WORKSPACE/openwrt
 [ -d openwrt ] && cd openwrt || exit
 echo "OPENWRT_PATH=$PWD" >>$GITHUB_ENV
 
-# 开始生成全局变量
+# 生成全局变量
 begin_time=$(date '+%H:%M:%S')
 [ -e $GITHUB_WORKSPACE/$CONFIG_FILE ] && cp -f $GITHUB_WORKSPACE/$CONFIG_FILE .config
 make defconfig 1>/dev/null 2>&1
@@ -222,7 +220,7 @@ else
     echo "REBUILD_TOOLCHAIN=true" >>$GITHUB_ENV
 fi
 
-# 开始更新&安装插件
+# 更新&安装插件
 begin_time=$(date '+%H:%M:%S')
 ./scripts/feeds update -a 1>/dev/null 2>&1
 ./scripts/feeds install -a 1>/dev/null 2>&1
@@ -261,7 +259,7 @@ sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/root/usr/sha
 # unblockneteasemusic
 sed -i 's/解除网易云音乐播放限制/音乐解锁/g' feeds/luci/applications/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 
-# 开始加载个人设置
+# 加载个人设置
 begin_time=$(date '+%H:%M:%S')
 
 [ -e $GITHUB_WORKSPACE/files ] && mv $GITHUB_WORKSPACE/files files
@@ -293,7 +291,7 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 # 添加编译日期
 #echo "DISTRIB_DATE='R$(date +%y.%-m.%-d)'" >>package/base-files/files/etc/openwrt_release
 
-# 取消主题默认设置
+# 删除主题默认设置
 # find $destination_dir/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
 
 # 修改连接数
@@ -325,11 +323,6 @@ sed -i '/# timezone/i sed -i "s/\\(DISTRIB_DESCRIPTION=\\).*/\\1'\''ImmortalWrt 
 #sed -i '/# timezone/i sed -i "s/\\(DISTRIB_DESCRIPTION=\\).*/\\1'\''ImmortalWrt $(sed -n "s/DISTRIB_DESCRIPTION='\''ImmortalWrt \\([^ ]*\\) .*/\\1/p" /etc/openwrt_release)'\'',/" /etc/openwrt_release\nsource /etc/openwrt_release \&\& sed -i -e "s/distversion\\s=\\s\\".*\\"/distversion = \\"$DISTRIB_ID $DISTRIB_RELEASE ($DISTRIB_REVISION)\\"/g" -e '\''s/distname    = .*$/distname    = ""/g'\'' /usr/lib/lua/luci/version.lua\nsed -i "s/luciname    = \\".*\\"/luciname    = \\"LuCI Master\\"/g" /usr/lib/lua/luci/version.lua\nsed -i "s/luciversion = \\".*\\"/luciversion = \\"v'$(date +%Y%m%d)'\\"/g" /usr/lib/lua/luci/version.lua\necho "export const revision = '\''v'$(date +%Y%m%d)'\'\'', branch = '\''LuCI Master'\'';" > /usr/share/ucode/luci/version.uc\n/etc/init.d/rpcd restart\n' package/emortal/default-settings/files/99-default-settings
 curl -fsSL https://raw.githubusercontent.com/0118Add/Openwrt-CI/main/immortalwrt/release-os > package/base-files/files/etc/os-release
 
-# rust
-pushd feeds/packages
-  curl -s https://patch-diff.githubusercontent.com/raw/openwrt/packages/pull/27487.patch | patch -p1
-popd
-
 # 修复 Makefile 路径
 find $destination_dir/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i \
     -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
@@ -345,7 +338,13 @@ for e in $(ls -d $destination_dir/luci-*/po feeds/luci/applications/luci-*/po); 
 done
 status "加载个人设置"
 
-# 开始下载openchash运行内核
+# 更新配置文件
+begin_time=$(date '+%H:%M:%S')
+[ -e $GITHUB_WORKSPACE/$CONFIG_FILE ] && cp -f $GITHUB_WORKSPACE/$CONFIG_FILE .config
+make defconfig 1>/dev/null 2>&1
+status "更新配置文件"
+
+# 下载openchash运行内核
 #[[ $CLASH_KERNEL =~ amd64|arm64|armv7|armv6|armv5|386 ]] && {
 #    begin_time=$(date '+%H:%M:%S')
 #    chmod +x $GITHUB_WORKSPACE/scripts/preset-clash-core.sh
@@ -353,7 +352,7 @@ status "加载个人设置"
 #    status "下载openchash运行内核"
 #}
 
-# 开始下载zsh终端工具
+# 下载zsh终端工具
 [[ $ZSH_TOOL = 'true' ]] && {
     begin_time=$(date '+%H:%M:%S')
     chmod +x $GITHUB_WORKSPACE/scripts/preset-terminal-tools.sh
@@ -361,19 +360,13 @@ status "加载个人设置"
     status "下载zsh终端工具"
 }
 
-# 开始下载adguardhome运行内核
+# 下载adguardhome运行内核
 #[[ $CLASH_KERNEL =~ amd64|arm64|armv7|armv6|armv5|386 ]] && {
 #    begin_time=$(date '+%H:%M:%S')
 #    chmod +x $GITHUB_WORKSPACE/scripts/preset-adguard-core.sh
 #    $GITHUB_WORKSPACE/scripts/preset-adguard-core.sh $CLASH_KERNEL
 #    status "下载adguardhome运行内核"
 #}
-
-# 开始更新配置文件
-begin_time=$(date '+%H:%M:%S')
-[ -e $GITHUB_WORKSPACE/$CONFIG_FILE ] && cp -f $GITHUB_WORKSPACE/$CONFIG_FILE .config
-make defconfig 1>/dev/null 2>&1
-status "更新配置文件"
 
 echo -e "$(color cy 当前编译机型) $(color cb $SOURCE_REPO-${REPO_BRANCH#*-}-$DEVICE_TARGET-$KERNEL_VERSION)"
 
