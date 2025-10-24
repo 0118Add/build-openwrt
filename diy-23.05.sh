129b87a5db3fb91ab3cd3dbe5d7108d0c5142491
0118Add <60157610+0118Add@users.noreply.github.com>
2025-10-25 00:47:47 +0800

    Update diy-23.05.sh

--- diy-23.05.sh
+++ diy-23.05.sh
@@ -300,7 +300,7 @@ sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/confi
 
 # 更改固件版本信息
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION=''|g" package/base-files/files/etc/openwrt_release
-sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='OpenWrt 23.05.6 / LuCI openwrt-23.05'|g" package/base-files/files/etc/openwrt_release
+sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='OpenWrt 23.05.7 / LuCI openwrt-23.05'|g" package/base-files/files/etc/openwrt_release
 
 # 删除主题默认设置
 # find $destination_dir/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
