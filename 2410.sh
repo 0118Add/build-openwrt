438285cacd2ef75fabfe2baa516c200fffcebd11
0118Add <60157610+0118Add@users.noreply.github.com>
2026-02-09 13:30:34 +0800

    Update 2410.sh

--- 2410.sh
+++ 2410.sh
@@ -373,11 +373,14 @@ apply_custom_settings() {
     sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
     # 调整Dockerman到服务菜单
-    #sed -i 's/"admin",/"admin","services",/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
-    #sed -i 's/"admin/"admin\/services/g' feeds/luci/applications/luci-app-dockerman/luasrc/model/*.lua
-    #sed -i 's/"admin/"admin\/services/g' feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
-    #sed -i 's/"admin/"admin\/services/g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
-    #sed -i 's/"admin/"admin\/services/g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/cbi/*.htm
+    rm -rf feeds/luci/applications/luci-app-dockerman
+    git clone https://github.com/lisaac/luci-app-dockerman package/dockerman
+    git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+    sed -i 's/"admin",/"admin","services",/g' package/dockerman/applications/luci-app-dockerman/luasrc/controller/*.lua
+    sed -i 's/"admin/"admin\/services/g' package/dockerman/applications/luci-app-dockerman/luasrc/model/*.lua
+    sed -i 's/"admin/"admin\/services/g' package/dockerman/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
+    sed -i 's/"admin/"admin\/services/g' package/dockerman/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
+    sed -i 's/"admin/"admin\/services/g' package/dockerman/applications/luci-app-dockerman/luasrc/view/dockerman/cbi/*.htm
 
     # 自定义默认配置
     sed -i '/exit 0$/d' package/emortal/default-settings/files/99-default-settings
