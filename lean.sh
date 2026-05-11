1ffbc2e17852a8c799d4e1202059a33c3a6b4d88
0118Add <60157610+0118Add@users.noreply.github.com>
2026-05-11 20:00:34 +0800

    Update lean.sh

--- lean.sh
+++ lean.sh
@@ -231,7 +231,7 @@ clone_source_code() {
     echo "OPENWRT_PATH=$PWD" >> $GITHUB_ENV
 
     # 设置luci版本为18.06
-    sed -i '/luci/s/^#//; /luci.git;openwrt/s/^/#/' feeds.conf.default
+    #sed -i '/luci/s/^#//; /luci.git;openwrt/s/^/#/' feeds.conf.default
 }
 
 # 设置环境变量
@@ -379,11 +379,7 @@ apply_custom_settings() {
     sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
     
     # 修改x86内核版本
-    KERNEL_PATCHVER=$(cat target/linux/x86/Makefile|grep KERNEL_PATCHVER | sed 's/^.\{17\}//g')
-    KERNEL_TESTING_PATCHVER=$(cat target/linux/x86/Makefile|grep KERNEL_TESTING_PATCHVER | sed 's/^.\{25\}//g')
-    if [[ $KERNEL_TESTING_PATCHVER > $KERNEL_PATCHVER ]]; then
-      sed -i "s/$KERNEL_PATCHVER/$KERNEL_TESTING_PATCHVER/g" target/linux/x86/Makefile
-    fi
+    sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.18/g' ./target/linux/x86/Makefile
     
     # 更改默认shell为zsh
     # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
