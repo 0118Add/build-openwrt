89a3bcf45d490fdb485b3e1e0dac6d1ef2c74316
0118Add <60157610+0118Add@users.noreply.github.com>
2026-07-29 18:20:13 +0800

    Update 02_target_only.sh

--- SCRIPTS/02_target_only.sh
+++ SCRIPTS/02_target_only.sh
@@ -13,13 +13,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # 修改主机名
 #sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 # 修改型号
-sed -i 's/echo.*/echo "$vendor" > \/tmp\/sysinfo\/model/g' target/linux/x86/base-files/lib/preinit/01_sysinfo
-# 更改固件版本信息
-#sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION=''|g" package/base-files/files/etc/openwrt_release
-sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='OpenWrt %V'|g" package/base-files/files/etc/openwrt_release
-# 修改 luci version.lua
-sed -i '/luciversion/d' feeds/luci/modules/luci-lua-runtime/luasrc/version.lua
-echo "luciversion = 'LuCI openwrt-25.12 branch'" >> feeds/luci/modules/luci-lua-runtime/luasrc/version.lua
+#sed -i 's/echo.*/echo "$vendor" > \/tmp\/sysinfo\/model/g' target/linux/x86/base-files/lib/preinit/01_sysinfo
 
 # remove LRNG for 3328
 rm -f target/linux/generic/hack-${KERNEL_VERSION}/696*
