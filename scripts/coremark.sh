02294678b90cf565cd8d153508cddad8bb68a938
0118Add <60157610+0118Add@users.noreply.github.com>
2026-07-31 14:24:23 +0800

    Update coremark.sh

--- scripts/coremark.sh
+++ scripts/coremark.sh
@@ -2,7 +2,7 @@
 
 /bin/coremark > /tmp/coremark.log
 
-cat /tmp/coremark.log | grep "CoreMark 1.0" | awk -F'.' '{print $1}' | cut -d  "/"  -f 1 > /etc/bench.log
+cat /tmp/coremark.log | grep "CoreMark 1.0" | cut -d  "/"  -f 1 > /etc/bench.log
 sed -i 's/CoreMark 1.0/ (CpuMark/g'  /etc/bench.log
 echo " Scores)" >> /etc/bench.log
 
